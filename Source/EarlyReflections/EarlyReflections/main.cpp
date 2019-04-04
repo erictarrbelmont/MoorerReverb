185b351687bc541a756cae70e69df1dd41f6dbba
Victor Pacek <victor.pacek@pop.belmont.edu>
2019-04-04 13:48:08 -0500

    Started class

---
+++ Source/EarlyReflections/EarlyReflections/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  EarlyReflections
+//
+//  Created by Victor Pacek on 4/4/19.
+//  Copyright © 2019 Victor Pacek. All rights reserved.
+//
+
+#include <iostream>
+#include math.h
+
+class EarlyReflections {
+    int delayBufferLength = 3520;
+    float Fs = 48000;
+    int tapTimes[18] = [190,759,44,190,9,123,706,119,384,66,35,75,419,4,79,66,53,194];
+    float tapGains[18] = [.841,.504,.49,.379,.38,.346,.289,.272,.192,.193,.217,.181,.18,.181,.176,.142,.167,.134,];
+    
+public:
+    setFs(){
+        
+    }
+    getFs(){
+        return Fs;
+    }
+    
+    EarlyReflectionsOut(){
+        pro
+    }
+    
+};
