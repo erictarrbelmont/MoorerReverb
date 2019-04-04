70f0280f278667c1a0b07969e2de591f117f6c34
MarkBlackham <markblackham@gmail.com>
2019-04-04 13:48:53 -0500

    Initial Commit FBCF

---
+++ Source/FBCF.cpp
@@ -0,0 +1,8 @@
+//
+//  FBCF.cpp
+//  MoorerReverb - AU
+//
+//  Created by Mark Blackham on 4/4/19.
+//
+
+#include "FBCF.hpp"
---
+++ Source/FBCF.hpp
@@ -0,0 +1,13 @@
+//
+//  FBCF.hpp
+//  MoorerReverb - AU
+//
+//  Created by Mark Blackham on 4/4/19.
+//
+
+#ifndef FBCF_hpp
+#define FBCF_hpp
+
+#include <stdio.h>
+
+#endif /* FBCF_hpp */
