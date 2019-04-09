9043aa2495f89b012baf9c6025b7f41609cb0881
BrianC <Brian@Brians-MacBook-Pro-2.local>
2019-04-09 13:06:17 -0500

    Initial GUI

---
+++ JuceLibraryCode/include_hack_audio_gui.mm
@@ -0,0 +1,9 @@
+/*
+
+    IMPORTANT! This file is auto-generated each time you save your
+    project - if you alter its contents, your changes may be overwritten!
+
+*/
+
+#include "AppConfig.h"
+#include <hack_audio_gui/hack_audio_gui.mm>
--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -18,6 +18,67 @@ MoorerReverbAudioProcessorEditor::MoorerReverbAudioProcessorEditor (MoorerReverb
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+    
+    //reverb time, diffusion, modulation knobs
+    
+    reverbTimeSlider.addListener(this);
+    //hpfSlider.setBounds(225,100,80,80);
+    reverbTimeSlider.setSize(sliderSize,sliderSize);
+    reverbTimeSlider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    reverbTimeSlider.setRange(0, 1); // slider can have values between 0 and 1
+    reverbTimeSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 60, 15);
+    reverbTimeSlider.setValue(1.0f);
+    addAndMakeVisible(reverbTimeSlider);
+    
+    reverbTimeLabel.setText("Reverb Time", dontSendNotification);
+    //hpfSliderLabel(HackAudio::Fonts::NowRegular);
+    //hpfSliderLabel(juce::Label::textColourId, HackAudio::Colours::White);
+    reverbTimeLabel.setJustificationType(Justification::centred);
+    reverbTimeLabel.attachToComponent(&reverbTimeSlider, false);
+    // 'false' is to put it on top
+    addAndMakeVisible(reverbTimeLabel);
+    
+    diffusionSlider.addListener(this);
+    //freqSlider.setBounds(225,10,80,80);
+    diffusionSlider.setSize(sliderSize,sliderSize);
+    diffusionSlider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    diffusionSlider.setRange(0.1f, 10.0f); // slider can have values between 0 and 1
+    diffusionSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 60, 15);
+    diffusionSlider.setValue(1.0f);
+    addAndMakeVisible(diffusionSlider);
+    
+    diffusionSliderLabel.setText("Diffusion", dontSendNotification);
+    //freqSliderLabel(HackAudio::Fonts::NowRegular);
+    //freqSliderLabel(juce::Label::textColourId, HackAudio::Colours::White);
+    diffusionSliderLabel.setJustificationType(Justification::centred);
+    diffusionSliderLabel.attachToComponent(&diffusionSlider, false);
+    // 'false' is to put it on top
+    addAndMakeVisible(diffusionSliderLabel);
+    
+    modulationSlider.addListener(this);
+    //depthSlider.setBounds(25,10,80,80);
+    modulationSlider.setSize(sliderSize,sliderSize);
+    modulationSlider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    modulationSlider.setRange(0.0f, 1.0f); // slider can have values between 0 and 1
+    modulationSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 60, 15);
+    modulationSlider.setValue(1.0f);
+    addAndMakeVisible(modulationSlider);
+    
+    modulationSliderLabel.setText("Modulation", dontSendNotification);
+    //depthSliderLabel(HackAudio::Fonts::NowRegular);
+    //depthSliderLabel(juce::Label::textColourId, HackAudio::Colours::White);
+    modulationSliderLabel.setJustificationType(Justification::centred);
+    modulationSliderLabel.attachToComponent(&modulationSlider, false);
+    // 'false' is to put it on top
+    addAndMakeVisible(modulationSliderLabel);
+
+    //Flex box for knobs
+    flexSliders.addComponent(reverbTimeSlider);
+    flexSliders.addComponent(diffusionSlider);
+    flexSliders.addComponent(modulationSlider);
+    
+    flexSliders.applyBounds(juce::Rectangle<int>(20,150,(4*sliderSize)+40,sliderSize));
+
 }
 
 MoorerReverbAudioProcessorEditor::~MoorerReverbAudioProcessorEditor()
@@ -39,4 +100,38 @@ void MoorerReverbAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+}
+
+void MoorerReverbAudioProcessorEditor::sliderValueChanged(Slider* slider){
+
+    
+    if (slider == &reverbTimeSlider){
+//        processor.hpf1.setAmount(slider->getValue());
+//        processor.softClip1.setDrive(slider->getValue());
+//        reverbTimeLabel.setText(String(hpfSlider.getValue(),1),dontSendNotification);
+    }
+    
+    if (slider == &diffusionSlider){
+//        *processor.tremoloFreq = slider->getValue();
+//        diffusionSliderLabel.setText(String(freqSlider.getValue(),1),dontSendNotification);
+    }
+    
+    if (slider == &modulationSlider){
+//        *processor.tremoloDepth = slider->getValue();
+//        modulationSliderLabel.setText(String(depthSlider.getValue(),1),dontSendNotification);
+    }
+    
+    startTimer(1200);
+    
+    
+}
+
+void MoorerReverbAudioProcessorEditor::timerCallback()
+{
+    stopTimer();
+   
+    reverbTimeLabel.setText("Reverb Time", dontSendNotification);
+    diffusionSliderLabel.setText("Diffusion", dontSendNotification);
+    modulationSliderLabel.setText("Modulation", dontSendNotification);
+    
 }
